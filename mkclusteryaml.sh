24de13a1e21ea95b12a71526bddb17c7f23b8b01
David Asabina <david@supr.nu>
2016-08-09 03:32:33 +0200

    Quote fixes

--- mkclusteryaml.sh
+++ mkclusteryaml.sh
@@ -1,9 +1,9 @@
 source settings.sh
 
 kube-aws init \
-  --cluster-name=\"$CLUSTER_NAME bootstrap experimentation cluster\" \
-  --external-dns-name=\"$CLUSTER_NAME\" \
-  --region=\"$CLUSTER_REGION\" \
-  --availability-zone=\"$CLUSTER_AVAILABILITY_ZONE\" \
-  --key-name=\"$CLUSTER_EC2_KEYPAIR\" \
-  --kms-key-arn=\"$KMS_KEY_ARN\"
+  --cluster-name=$CLUSTER_NAME \
+  --external-dns-name=$CLUSTER_DNS_NAME \
+  --region=$CLUSTER_REGION \
+  --availability-zone=$CLUSTER_AVAILABILITY_ZONE \
+  --key-name=$CLUSTER_EC2_KEYPAIR \
+  --kms-key-arn="$KMS_KEY_ARN"
--- settings.sh
+++ settings.sh
@@ -1,7 +1,8 @@
-KMS_DESCRIPTION="kube-aws assets"
+KMS_DESCRIPTION=kube-aws assets
 KMS_KEY_ARN="$(cat aws.kms.arn)"
 
-CLUSTER_NAME="orion"
-CLUSTER_REGION="eu-central-1"
-CLUSTER_AVAILABILITY_ZONE="eu-central-1a"
-CLUSTER_EC2_KEYPAIR="tmh.k8s.supr.nu"
+CLUSTER_NAME=orion-k8s-experiment
+CLUSTER_DNS_NAME=orion
+CLUSTER_REGION=eu-central-1
+CLUSTER_AVAILABILITY_ZONE=eu-central-1a
+CLUSTER_EC2_KEYPAIR=tmh.k8s.supr.nu
